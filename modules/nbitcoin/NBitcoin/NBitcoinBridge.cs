1a6b76e3fbf67b72ce30ba25174b055fc6517776
Ondřej Češka <xceska2@fi.muni.cz>
2025-11-27 14:39:27 +0100

    add testnet support and envvar to nbitcoin

--- modules/nbitcoin/NBitcoin/NBitcoinBridge.cs
+++ modules/nbitcoin/NBitcoin/NBitcoinBridge.cs
@@ -68,40 +68,84 @@ public static class Bridge
             input = Marshal.PtrToStringUTF8(inputPtr);
             if (string.IsNullOrEmpty(input))
             {
-                return Marshal.StringToCoTaskMemUTF8("could not convert to string");
+                return Marshal.StringToCoTaskMemUTF8("INVALID");
             }
         }
         catch
         {
-            return Marshal.StringToCoTaskMemUTF8("could not convert to string");
+            return Marshal.StringToCoTaskMemUTF8("INVALID");
         }
-
-        if (TryParseXprv(input, out string xprvResult)){
-            return Marshal.StringToCoTaskMemUTF8(xprvResult);
+        //get environment variable EXTKEYTYPE
+        string extKeyType = Environment.GetEnvironmentVariable("EXTKEYTYPE");
+        if (extKeyType != "xpub" && extKeyType != "xprv" &&
+            extKeyType != "tpub" && extKeyType != "tprv")
+        {
+            extKeyType = "xpub";
         }
-
-        if (TryParseXpub(input, out string xpubResult)){
-            return Marshal.StringToCoTaskMemUTF8(xpubResult);
+        //try parse based on extKeyType
+        if (extKeyType == "xprv")
+        {
+            if (TryParseXprv(input, Network.Main, out string xprvResult)){
+                Console.WriteLine("Parsed as xprv");
+                Console.WriteLine(xprvResult);
+                return Marshal.StringToCoTaskMemUTF8(xprvResult);
+            }
+            else{
+                return Marshal.StringToCoTaskMemUTF8("INVALID");;
+            }
         }
-
-        return Marshal.StringToCoTaskMemUTF8("INVALID");
-
+        else if (extKeyType == "tprv")
+        {
+            if (TryParseXprv(input, Network.TestNet, out string tprvResult)){
+                Console.WriteLine("Parsed as tprv");
+                Console.WriteLine(tprvResult);
+                return Marshal.StringToCoTaskMemUTF8(tprvResult);
+            }
+            else{
+                return Marshal.StringToCoTaskMemUTF8("INVALID");;
+            }
+        }
+        else if (extKeyType == "xpub")
+        {
+            if (TryParseXpub(input, Network.Main, out string xpubResult)){
+                Console.WriteLine("Parsed as xpub");
+                Console.WriteLine(xpubResult);
+                return Marshal.StringToCoTaskMemUTF8(xpubResult);
+            }
+            else{
+                return Marshal.StringToCoTaskMemUTF8("INVALID");;
+            }
+        }
+        else //tpub
+        {
+            if (TryParseXpub(input, Network.TestNet, out string tpubResult)){
+                Console.WriteLine("Parsed as tpub");
+                Console.WriteLine(tpubResult);
+                return Marshal.StringToCoTaskMemUTF8(tpubResult);
+            }
+            else{
+                return Marshal.StringToCoTaskMemUTF8("INVALID");;
+            }
+        }
     }
     // Helper methods for BIP32 deserialization...
+
+    private static string Hex(byte[] data) =>
+        Convert.ToHexString(data).ToLower();
+
     // REFACTOR to cleaner code?
-    private static bool TryParseXprv(string input, out string result)
+    private static bool TryParseXprv(string input,Network network, out string result)
     {
         result = null;
         try
         {
-            var ext = NBitcoin.ExtKey.Parse(input, Network.Main);
+            var ext = NBitcoin.ExtKey.Parse(input, network);
 
-            string depthHex = ext.Depth.ToString("X2").ToLower();
-            byte[] fingerprint = ext.ParentFingerprint.ToBytes();
-            string fingerprintHex = BitConverter.ToString(fingerprint).Replace("-", "").ToLower();
-            string childHex = ext.Child.ToString("X8").ToLower();
-            string chainHex = BitConverter.ToString(ext.ChainCode).Replace("-", "").ToLower();
-            string keyHex = BitConverter.ToString(ext.PrivateKey.ToBytes()).Replace("-", "").ToLower();
+            string depthHex = ext.Depth.ToString("x2");
+            string fingerprintHex = Hex(ext.ParentFingerprint.ToBytes());
+            string childHex = ext.Child.ToString("x8");
+            string chainHex = Hex(ext.ChainCode);
+            string keyHex = ext.PrivateKey.ToHex().ToLower();
 
             result = $"depth={depthHex};fp={fingerprintHex};child={childHex};chaincode={chainHex};key={keyHex}";
             return true;
@@ -112,18 +156,17 @@ public static class Bridge
         }
     }
 
-    private static bool TryParseXpub(string input, out string result)
+    private static bool TryParseXpub(string input,Network network,out string result)
     {
         result = null;
         try
         {
-            var exp = NBitcoin.ExtPubKey.Parse(input, Network.Main);
+            var exp = NBitcoin.ExtPubKey.Parse(input, network);
 
-            string depthHex = exp.Depth.ToString("X2").ToLower();
-            byte[] fingerprint = exp.ParentFingerprint.ToBytes();
-            string fingerprintHex = BitConverter.ToString(fingerprint).Replace("-", "").ToLower();
-            string childHex = exp.Child.ToString("X8").ToLower();
-            string chainHex = BitConverter.ToString(exp.ChainCode).Replace("-", "").ToLower();
+            string depthHex = exp.Depth.ToString("x2");
+            string fingerprintHex = Hex(exp.ParentFingerprint.ToBytes());
+            string childHex = exp.Child.ToString("x8");
+            string chainHex = Hex(exp.ChainCode);
             string pubKeyHex = exp.PubKey.ToHex().ToLower();
 
             result = $"depth={depthHex};fp={fingerprintHex};child={childHex};chaincode={chainHex};pub={pubKeyHex}";
